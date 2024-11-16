5b1a33124b15f0011d9e02e1af024431b7b3bc00
Ajinkya Joshi <aaj.ajinkya@gmail.com>
2024-11-16 13:11:52 +0530

    second branch inital commit

---
+++ bubble_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// function to swap
+
+
+
+// function for bubble sort
+
+
+// main function
+int main(){
+    int arr[7] = {3,7,12,4,8,1,5};
+    int n= 7;
+    printf("Initial array: ");
+    for (int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
