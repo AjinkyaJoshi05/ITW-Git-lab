9a7fe2810310400427bdd468e65e3dfc1adc28b3
Ajinkya Joshi <aaj.ajinkya@gmail.com>
2024-11-16 12:47:14 +0530

    inital commit

---
+++ insertion_sort.c
@@ -0,0 +1,10 @@
+
+// make an array of numbers
+// write a function for selection sort
+// sort the array using selection sort
+
+// function for insertion sort
+
+
+// function to print the array
+
