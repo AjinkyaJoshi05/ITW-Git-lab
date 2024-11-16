6bf45114ebfc8bb0225671c92d7b0d943b91ac2a
Ajinkya Joshi <aaj.ajinkya@gmail.com>
2024-11-16 12:49:54 +0530

    driver code and utility function to print array

--- insertion_sort.c
+++ insertion_sort.c
@@ -8,3 +8,21 @@
 
 // function to print the array
 
+void print(int arr[],int n){
+    printf("Array: ");
+    for (int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+    printf("Insertion sort\n");
+    int arr[7] = {3,7,12,4,8,1,5};
+    int n= 7;
+    printf("Initial array: ");
+    print(arr,n);
+    insertionSort(arr,n);
+    printf("\nSorted array: ");
+    print(arr,n);
+    return 0;
+}
